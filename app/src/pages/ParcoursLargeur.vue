12e30d005769055e18d07692ba6878e4c83a46ec
Marine Legendre <marine.leg02@gmail.com>
2024-01-08 21:04:40 +0100

    wip: création du graph à partir d'une matrice donnée

--- app/src/pages/ParcoursLargeur.vue
+++ app/src/pages/ParcoursLargeur.vue
@@ -1,17 +1,16 @@
 <script setup lang="js">
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
-import data from "../tree/graph_oriente"
-import dagre from "dagre/dist/dagre.min.js"
-import * as vNG from "v-network-graph"
+import {createGraphFromMatrix } from "../utils/createGraphFromMatrix"
 
+// fonction exécutant le script Lua via une requête HTTP
 const runLuaScript = async () => {
   try {
-    // Effectuer une requête POST pour exécuter le script Lua
     const response = await axios.post('http://localhost:3000/', {
-      script: 'parcours_largeur', // nom exact du script
+      script: 'parcours_largeur', // Nom exact du script Lua à exécuter (/api/scripts/)
       data: { makey: 'mavaleur', makey2: 'maval2' },
     });
+    // Transforme le résultat en tableau correspondant au différent print() du script Lua
     return response.data.result.split('\r\n')
   } catch (error) {
     console.error('Error calling the Lua script:', error);
@@ -19,92 +18,40 @@ const runLuaScript = async () => {
 };
 
 const res = ref('')
+
+// Appelle la fonction runLuaScript au moment du montage du composant
 onMounted(async () => {
   res.value = await runLuaScript();
 });
 
-const nodeSize = 40
-
-const configs = vNG.defineConfigs({
-  view: {
-    scalingObjects: true,
-    autoPanAndZoomOnLoad: "fit-content",
-    onBeforeInitialDisplay: () => layout("LR"),
-  },
-  node: {
-    normal: { radius: nodeSize / 2 },
-    label: { direction: "center", color: "#fff" },
-  },
-  edge: {
-    normal: {
-      color: "#aaa",
-      width: 3,
-    },
-    margin: 4,
-    marker: {
-      target: {
-        type: "arrow",
-        width: 4,
-        height: 4,
-      },
-    },
-  },
-})
-
-// place les noeuds automatiquement sans chevauchement
-function layout(direction) {
-  if (Object.keys(data.nodes).length <= 1 || Object.keys(data.edges).length == 0) {
-    return
-  }
-
-  // convert graph
-  // ref: https://github.com/dagrejs/dagre/wiki
-  const g = new dagre.graphlib.Graph()
-  // Set an object for the graph label
-  g.setGraph({
-    rankdir: direction,
-    nodesep: nodeSize * 2,
-    edgesep: nodeSize,
-    ranksep: nodeSize * 2,
-  })
-  // Default to assigning a new object as a label for each new edge.
-  g.setDefaultEdgeLabel(() => ({}))
-
-  // Add nodes to the graph. The first argument is the node id. The second is
-  // metadata about the node. In this case we're going to add labels to each of
-  // our nodes.
-  Object.entries(data.nodes).forEach(([nodeId, node]) => {
-    g.setNode(nodeId, { label: node.name, width: nodeSize, height: nodeSize })
-  })
-
-  // Add edges to the graph.
-  Object.values(data.edges).forEach(edge => {
-    g.setEdge(edge.source, edge.target)
-  })
-
-  dagre.layout(g)
-
-  g.nodes().forEach((nodeId) => {
-    // update node position
-    const x = g.node(nodeId).x
-    const y = g.node(nodeId).y
-    data.layouts.nodes[nodeId] = { x, y }
-  })
-}
+// Matrice d'adjacence représentant le graphe
+const adjacencyMatrix = [
+  [0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1],
+  [0, 1, 0, 0, 0, 0],
+];
+// Appelle la fonction createGraphFromMatrix pour obtenir les données du graphe
+const { nodes, edges, layouts, configs } = createGraphFromMatrix(adjacencyMatrix);
 </script>
 
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Parcours en largeur </h5>
+
+    <!-- Graph -->
     <div class="graphContainer">
       <v-network-graph 
         class="graph"
-        :nodes="data.nodes" 
-        :edges="data.edges" 
-        :layouts="data.layouts" 
+        :nodes="nodes" 
+        :edges="edges" 
+        :layouts="layouts" 
         :configs="configs" 
       />
     </div>
+    <!-- Résultat du script Lua -->
     <p>{{ res[0] }}</p>
     <p>{{ res[1] }}</p>
   </q-page>
---
+++ app/src/utils/createGraphFromMatrix.js
@@ -0,0 +1,128 @@
+import { reactive } from "vue";
+import * as vNG from "v-network-graph"; // dessin et config du graph
+import dagre from "dagre/dist/dagre.min.js"; // disposition auto des noeuds du graph
+
+/**
+ * Construire les nœuds et les arêtes à partir d'une matrice d'adjacence
+ * @param {Array} matrix 
+ * @returns nodes et edges
+ */
+function buildNodesAndEdgesFromMatrix(matrix) {
+  const nodes = {};
+  const edges = {};
+
+   // Parcourt des lignes de la matrice pour créer les noeuds
+   for (let rowId = 0; rowId < matrix.length; rowId++) {
+    // ASCII 65 = A --> on incrément le numéro ASCII pour avoir A...Z
+    const sourceNode = String.fromCharCode(65 + rowId);
+    nodes[sourceNode] = { name: sourceNode };
+
+    // Parcourt des colonnes de matrice pour créer les arêtes
+    for (let colId = 0; colId < matrix[rowId].length; colId++) {
+      const value = matrix[rowId][colId];
+      if (value === 1) {
+        const targetNode = String.fromCharCode(65 + colId);
+        // Crée un nom d'arête unique
+        const edgeName = `edge${Object.keys(edges).length + 1}`;
+        // Ajoute l'arête à l'objet des arêtes
+        edges[edgeName] = { source: sourceNode, target: targetNode };
+      }
+    }
+  }
+
+  return { nodes, edges };
+}
+
+/**
+ * Créer le graphe à partir de la matrice d'adjacence
+ * @param {Array} adjacencyMatrix 
+ * @returns 
+ */
+function createGraphFromMatrix(adjacencyMatrix) {
+  // récupère les noeuds et les arrêtes à partir de la matrice d'adjacence
+  const { nodes, edges } = buildNodesAndEdgesFromMatrix(adjacencyMatrix);
+  // va stocker les positions des noeuds
+  const layouts = reactive({
+    nodes: {},
+  });
+  // définit la taille des noeuds
+  const nodeSize = 40;
+
+  // Définit les configurations du graphe avec v-network-graph
+  // https://dash14.github.io/v-network-graph/reference/configurations.html
+  const configs = vNG.defineConfigs({
+    view: {
+      scalingObjects: true,
+      autoPanAndZoomOnLoad: "fit-content",
+      // Appelle layout() avant l'affichage initial avec une direction spécifiée (horizontale)
+      onBeforeInitialDisplay: () => layout("LR"),
+    },
+    node: {
+      normal: { radius: nodeSize / 2 },
+      label: { direction: "center", color: "#fff" },
+    },
+    edge: {
+      normal: {
+        color: "#aaa",
+        width: 3,
+      },
+      margin: 4,
+      marker: {
+        target: {
+          type: "arrow",
+          width: 4,
+          height: 4,
+        },
+      },
+    },
+  });
+
+  /**
+   * Effectue la disposition des nœuds
+   * @param {String} direction LR pour horizontale, TB pour verticale
+   * @returns 
+   */
+  function layout(direction) {
+    // Vérifie s'il y a plus d'un nœud et au moins une arête
+    if (Object.keys(nodes).length <= 1 || Object.keys(edges).length === 0) {
+      return;
+    }
+
+    // Crée un objet graphique avec la bibliothèque dagre
+    const g = new dagre.graphlib.Graph();
+    // paramètres de disposition
+    g.setGraph({
+      rankdir: direction,
+      nodesep: nodeSize * 2,
+      edgesep: nodeSize,
+      ranksep: nodeSize * 2,
+    });
+
+    // fonction par défaut pour les étiquettes des arêtes
+    g.setDefaultEdgeLabel(() => ({}));
+
+    // Ajoute les noeud au graphique avec les positions initiales
+    Object.entries(nodes).forEach(([nodeId, node]) => {
+      g.setNode(nodeId, { label: node.name, width: nodeSize, height: nodeSize });
+    });
+
+    // Ajoute les arêtes au graphique
+    Object.values(edges).forEach((edge) => {
+      g.setEdge(edge.source, edge.target);
+    });
+
+    // Effectue la disposition avec dagre
+    dagre.layout(g);
+
+    // Met à jour les positions des nœuds dans l'objet réactif
+    g.nodes().forEach((nodeId) => {
+      const x = g.node(nodeId).x;
+      const y = g.node(nodeId).y;
+      layouts.nodes[nodeId] = { x, y };
+    });
+  }
+
+  return { nodes, edges, layouts, configs };
+}
+
+export { createGraphFromMatrix };
