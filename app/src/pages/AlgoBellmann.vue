240c23c13a0c2e114063417293627b1edc425320
Marine Legendre <marine.leg02@gmail.com>
2024-02-07 23:29:56 +0100

    fix

--- app/src/pages/AlgoBellmann.vue
+++ app/src/pages/AlgoBellmann.vue
@@ -1,3 +1,51 @@
+<script setup lang="js">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import { createGraphFromMatrix } from "../utils/createGraphFromMatrix";
+
+// Define runLuaScript function
+const runLuaScript = async (matrix) => {
+  try {
+    const response = await axios.post('http://localhost:3000/', {
+      script: 'bellmann',
+      data: { "matrice": matrix },
+    });
+    return response.data.result.split('\r\n');
+  } catch (error) {
+    console.error('Error calling the Lua script:', error);
+  }
+};
+
+// Matrice de base
+const originalAdjacencyMatrix = ref([
+  [0, 5, 2, 0, 10, 0],
+  [0, 0, 0, 8, 0, 3],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 8],
+  [0, 0, 0, 2, 0, 0],
+]);
+
+const res = ref([]);
+const nodes = ref([]);
+const edges = ref([]);
+const layouts = ref([]);
+const configs = ref([]);
+
+async function updateGraph(){
+  const result = createGraphFromMatrix(originalAdjacencyMatrix.value, true);
+  nodes.value = result.nodes;
+  edges.value = result.edges;
+  layouts.value = result.layouts;
+  configs.value = result.configs;
+  res.value = await runLuaScript(originalAdjacencyMatrix.value);
+};
+
+onMounted(async () => {
+  await updateGraph();
+});
+</script>
+
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Bellmann</h5>
@@ -5,21 +53,15 @@
     <!-- Conteneur principal avec flexbox -->
     <div class="container">
 
-      <!-- Partie gauche : Matrice d'adjacence et Graph -->
+      <!-- Partie gauche : Matrice d'adjacence et Graph-->
       <div class="left">
-        <q-input
-          v-model="matrixText"
-          label="Matrice d'adjacence"
-          autogrow
-          outlined
-          readonly
-        />
 
-        <!-- Matrix Input for Modification -->
+        <!-- Matrice -->
+        <p class="title">Matrice</p>
         <div class="matrix-input">
-          <div v-for="(row, rowIndex) in adjacencyMatrix" :key="rowIndex" class="matrix-row">
+          <div v-for="(row, rowIndex) in originalAdjacencyMatrix" :key="rowIndex" class="matrix-row">
             <div v-for="(value, colIndex) in row" :key="colIndex" class="matrix-cell">
-              <q-input v-model="adjacencyMatrix[rowIndex][colIndex]" type="number" outlined dense />
+              <q-input v-model="originalAdjacencyMatrix[rowIndex][colIndex]" type="number" outlined dense />
             </div>
           </div>
           <q-btn @click="updateGraph" label="Update Graph" />
@@ -48,68 +90,21 @@
 
       <!-- Partie droite : Résultat du script Lua -->
       <div  class="right">
-        <p style="font-size:15pt ; font-weight: 600; color: #3355BB;" >Resultats</p>
+        <p class="title">Resultats</p>
         <p v-for="(result, index) in res" :key="index">{{ result }}</p>
       </div>
-
     </div>
-
   </q-page>
 </template>
 
-<script setup lang="js">
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { createGraphFromMatrix } from "../utils/createGraphFromMatrix";
-
-// Define runLuaScript function
-const runLuaScript = async (matrix) => {
-  try {
-    const response = await axios.post('http://localhost:3000/', {
-      script: 'bellmann',
-      data: { "matrice": matrix },
-    });
-    return response.data.result.split('\r\n');
-  } catch (error) {
-    console.error('Error calling the Lua script:', error);
-  }
-};
-
-const originalAdjacencyMatrix = [
-  [0, 5, 2, 0, 10, 0],
-  [0, 0, 0, 8, 0, 3],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 8],
-  [0, 0, 0, 2, 0, 0],
-];
-const adjacencyMatrix = JSON.parse(JSON.stringify(originalAdjacencyMatrix));
-
-const res = ref('');
-const{ nodes, edges, layouts, configs } = createGraphFromMatrix(originalAdjacencyMatrix, true);
-let matrixText = adjacencyMatrix.map(row => `[${row.join(', ')}]`).join(',\n');
-
-const updateGraph = async () => {
-  adjacencyMatrix.forEach((row, rowIndex) => {
-    row.forEach((value, colIndex) => {
-      adjacencyMatrix[rowIndex][colIndex] = parseFloat(adjacencyMatrix[rowIndex][colIndex]);
-    });
-  });
-
-   createGraphFromMatrix(adjacencyMatrix, true);
-  
-
-  res.value = await runLuaScript(adjacencyMatrix);
-  matrixText = adjacencyMatrix.map(row => `[${row.join(', ')}]`).join(',\n');
-};
-
-
-onMounted(async () => {
-  res.value = await runLuaScript(originalAdjacencyMatrix);
-});
-</script>
-
 <style>
+
+.title {
+  font-size:15pt; 
+  font-weight: 600; 
+  color: #3355BB;
+}
+
 .container {
   display: flex;
 }
