0bb426fb398beca96b860b50df92996741d71d06
Marine Legendre <marine.leg02@gmail.com>
2024-02-07 21:06:31 +0100

    supp fichier inutilisé

--- app/src/pages/AlgoKruskal.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <q-page class="q-pa-lg">
-  <h5 class="q-mt-none">Algorithme de Kruskal</h5>
-  </q-page>
-</template>
-
-<script setup lang="js">
-</script>
-
--- app/src/pages/AlgoPrim.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <q-page class="q-pa-lg">
-  <h5 class="q-mt-none">Algorithme de Prim</h5>
-  </q-page>
-</template>
-
-<script setup lang="js">
-</script>
-
